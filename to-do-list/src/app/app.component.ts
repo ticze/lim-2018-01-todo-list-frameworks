dc15a2213b1356edb88d75b6c90a512b8895648e
ticze <ticze2598@gmail.com>
2018-10-09 09:58:11 -0500

    creando list

--- to-do-list/src/app/app.component.ts
+++ to-do-list/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+class Todo {
+  description : string;
+  isDone : boolean;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'to-do-list';
+  public todos : Todo[] = [];
+  public description : string;
+  
+  addNewTodo(): void{
+    this.todos.push({
+      description: this.description,
+      isDone : false
+    })
+    this.description = null;
+  }
 }
